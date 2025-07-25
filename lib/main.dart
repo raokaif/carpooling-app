b53cd5eff34fe34d973a4a663567bd6a35eaeb3b
kaif <kaifrao250@gmail.com>
2025-07-25 15:22:55 +0500

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:carpooling_app/screens/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -11,9 +16,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Carpooling App',
+      theme: ThemeData(),
       debugShowCheckedModeBanner: false,
-      home: Placeholder(),
+      home: Auth(),
     );
   }
 }
